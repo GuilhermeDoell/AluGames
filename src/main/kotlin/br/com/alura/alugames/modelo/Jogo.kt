d829583ecd15ab4614dc8df1fb7ccfe0e1f68489
Guilherme Doell <guilhermedoell@outlook.com>
2024-08-26 16:58:58 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/alura/alugames/modelo/Jogo.kt
@@ -0,0 +1,13 @@
+package org.example
+
+data class Jogo(val titulo:String,
+           val capa:String) {
+    var descricao:String? = null
+    override fun toString(): String {
+        return "Meu jogo:\n" +
+                "Título: $titulo \n" +
+                "Capa: $capa \n" +
+                "Descricao: $descricao"
+    }
+
+}
